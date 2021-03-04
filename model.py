634528230db68b314a5953d4312db05dcd88040c
nuggy875 <jinugpaeng@gmail.com>
2021-03-04 15:18:58 +0000

    model start

---
+++ model.py
@@ -0,0 +1,15 @@
+
+class CSPDarknet53(nn.Module):
+
+    def __init__(self, block=ResBlock, num_classes=1000, pretrained=True):
+        super().__init__()
+        
+        self.num_classes = num_classes
+
+
+
+class YOLOv4(nn.Module):
+    def __init__(self):
+
+    def forward(self, x):
+
